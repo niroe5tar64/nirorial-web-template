d4d7385fd3dc06a5d507f006aaf624123a4761c0
niroe5tar64 <niroe5tar64@gmail.com>
2024-03-23 18:43:38 +0900

    [exec] `bun run biome:fix`

--- app/check.spec.ts
+++ app/check.spec.ts
@@ -1,8 +1,8 @@
 // app/check.spec.ts
 import { describe, it } from 'vitest'
 
-describe("check", () => {
-  it("show env without error", () => {
-    console.log(`env: ${process.env.NODE_ENV}`);
-  });
-});
+describe('check', () => {
+  it('show env without error', () => {
+    console.log(`env: ${process.env.NODE_ENV}`)
+  })
+})
